5edebbc989e50799b58f07a797d867a2968a9a11
Stepan_Suprunov <ipsuprunovso@mail.ru>
2022-06-16 17:44:22 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Измерение расстояние между двумя точками в 2D просторанстве.
+
+Console.WriteLine("Введите координаты точки А");
+Console.WriteLine("X:");
+int ax = int.Parse(Console.ReadLine());
+Console.WriteLine("Y:");
+int ay = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B");
+Console.WriteLine("X:");
+int bx = int.Parse(Console.ReadLine());
+Console.WriteLine("Y:");
+int by = int.Parse(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2));
+
+Console.WriteLine(Math.Round(result, 2));
